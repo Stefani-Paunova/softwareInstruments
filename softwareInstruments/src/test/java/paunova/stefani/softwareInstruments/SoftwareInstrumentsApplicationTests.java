90eb45121a0ffe006785d8973b8e29d4d665322c
Stefani-Paunova <stefanipaunova7@gmail.com>
2022-05-08 14:17:19 +0300

    [] initialize spring boot project

---
+++ softwareInstruments/src/test/java/paunova/stefani/softwareInstruments/SoftwareInstrumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package paunova.stefani.softwareInstruments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareInstrumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
